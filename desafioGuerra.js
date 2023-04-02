69eb88fb9657e88c1c3a8d7a0f9dbe18f67c174b
arturoguerrab <arturoguerrab12@gmail.com>
2023-04-02 03:21:25 -0300

    Codigo completo, Falta revisar y optimizar de ser posible

--- desafioGuerra.js
+++ desafioGuerra.js
@@ -1,23 +1,55 @@
 class ProductManager{
 
-    constructor (title,description,price,thumbnail,code,stock){
-        this.title=title;
-        this.description=description;
-        this.price=price;
-        this.thumbnail=thumbnail;
-        this.code=code;
-        this.stock=stock;
+    #products
+    constructor() {
+        this.#products = []
+        this.index = 0
     }
-    addProduct(cantidad){
-        this.stockReal= this.stockReal-cantidad;
+
+    addProduct(title,description,price,thumbnail,code,stock){
+
+        const product = {title,description,price,thumbnail,code,stock}
+
+        if (!title || !description || !price || !thumbnail || !code || !stock){
+            return console.log(`*\n<ERROR - Faltan datos para completar el registro del producto>\n*`)
+        }
+
+        const codeRepeat = this.#products.find(element=>element.code == product.code)
+        
+        if (codeRepeat){
+            return console.log(`*\n<ERROR - Producto repetido (code: ${codeRepeat.code}): No se agrego>\n*`)
+        }
+
+        this.index++
+        const id = this.index
+        this.#products.push({ id,...product})
+        
+
     }
-    getProducts(cantidad){
-        this.stockReal= this.stockReal-cantidad;
+    getProducts = () => {
+        return this.#products
     }
-    getProductById(cantidad){
-        this.stockReal= this.stockReal-cantidad;
+
+    getProductById(id){
+
+        const findProductById = this.#products.find(element=>element.id == id)
+
+        if (!findProductById){
+            return console.log('*\n<ERROR: Not found>\n*')
+        }
+
+        return findProductById
+
     }
+    
     
 }
 
-console.log("hola")
+const product = new ProductManager()
+
+product.addProduct('anti','vela de soja',800,'link',1005,255)
+product.addProduct('anti','vela de soja',800,'link',1005,800)
+product.addProduct('anti','vela de soja',800,'link',1006,500)
+product.addProduct('anti','vela de soja',800,'link',1008)
+
+console.log(product.getProducts())
