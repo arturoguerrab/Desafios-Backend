758670b570b3670fbe3f4bd6610dc1118f5c550f
arturoguerrab <arturoguerrab12@gmail.com>
2023-05-23 00:22:45 -0300

    Realtime en chat

--- src/Router/messages.router.js
+++ src/Router/messages.router.js
@@ -7,15 +7,17 @@ const router = Router()
 
 
 router.get('/', async (req,res)=>{ 
+    
     const historial= await Manager.getHistorial()
     res.render('messages',{historial}) 
 
 })
 
 router.post('/', async (req,res)=>{ 
+
     const message =req.body
-    console.log(message)
     await Manager.newMessage(message)
+    res.redirect("/chat")
 
 })
 
