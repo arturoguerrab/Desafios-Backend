ec0d30a1cc351ec0ad50b79f927b2e1cccb7e6d7
arturoguerrab <arturoguerrab12@gmail.com>
2023-06-05 21:41:37 -0300

    Desafio Clase 19: implementacion de login

---
+++ src/DAO/Models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userCollection = 'users'
+
+const userSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: String,
+    age: Number,
+    password:String,
+    rol:{ 
+        type:String,
+        default:'usuario'
+    }
+})
+
+mongoose.set('strictQuery',false)
+
+const userModel = mongoose.model(userCollection,userSchema)
+
+export default userModel
