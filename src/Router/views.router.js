2e752e75b2440900dc1e25c835cc8dfb97faeae1
arturoguerrab <arturoguerrab12@gmail.com>
2023-08-01 01:03:26 -0300

    Implementacion de logger

--- src/Router/views.router.js
+++ src/Router/views.router.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { generateProduct, passportCall } from "../utils.js";
 import { realTimeProducts, renderCart, renderPaginatedProducts, renderProducts } from "../controllers/views.controller.js";
+import logger from "../logger/logger.js";
+
 
 
 const router = Router()
@@ -20,6 +22,18 @@ router.get('/mockingproducts', async(req,res)=>{
         products.push(generateProduct())
     }
     res.send({ status: 'success', payload: products })
+})
+
+router.get('/loggertest', (req, res) => {
+
+    logger.debug('Prueba debug logger')
+    logger.http('Prueba http logger')
+    logger.info('Prueba info logger')
+    logger.warning('Prueba warning logger')
+    logger.error('Prueba error logger')
+    logger.fatal('Prueba fatal logger')
+
+    res.json({ status: 'success' })
 })
 
 export default router
