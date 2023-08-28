ec8872884cc35590dd58b41496f6d7b51e99f58f
arturoguerrab <arturoguerrab12@gmail.com>
2023-08-28 00:59:21 -0300

    Supertest Act

--- test/supertest.test.js
+++ test/supertest.test.js
@@ -1,9 +1,6 @@
 import { faker } from "@faker-js/faker";
 import chai from "chai";
 import supertest from "supertest";
-import { AdminPass, extractCookie, generateToken } from "../src/utils.js";
-import { ExtractJwt } from "passport-jwt";
-import passport from "passport";
 
 const expect = chai.expect
 const requester = supertest('http://localhost:8080')
@@ -42,40 +39,49 @@ describe('Testing router session', ()=>{
 })
 
 
-// describe('Testing router product', ()=>{
-//     it('Endpoint POST de api/products - Crear un nuevo producto', async()=>{
-//         const person = {
-//             first_name: faker.person.firstName(),
-//             last_name: faker.person.lastName(),
-//             email: "arturoguerraba@gmail.com",
-//             age: faker.string.numeric({length:2}),
-//             password: 'hola',
-//             rol:'admin'
-            
-//         }
-//         const product={
-//             title: "Anamorphine",
-//             description: "Peonías y Rosas Damascenas + Frambuesas",
-//             code: "1003",
-//             price: 798,
-//             status: true,
-//             stock: 70,
-//             category: "overose",
-//             thumbnails: "https://res.cloudinary.com/dbwomkmnq/image/upload/v1676337304/Whimsy/Tarjeta%20A/tarjeta_a_i1grfh.jpg"
-                
-//         }
+describe('Testing router product', ()=>{
+    it('Endpoint GET de api/products - Funcion de la paginacion de productos', async()=>{
         
-//         const token = generateToken(person)
-//             person.token = token
 
-
-//         const response = await requester.post(passport.authenticate('login'),'/api/products').send(product)
-//         const {text} =response
-
-//         // console.log(response);
+        const response = await requester.get('/api/products')
+        const {_body} =response
         
-//         // expect(text).to.be.eq('Found. Redirecting to /session/login')
+        expect(_body.status).to.be.eq('success')
 
-//     })
+    })
 
-// })
+    it('Endpoint GET de api/products - Traer producto por su id', async()=>{
+        
+
+        const response = await requester.get('/api/products/646bb430c4f8e37c6914cd7b')
+        const {_body} =response
+        
+        expect(_body.status).to.be.eq('success')
+
+    })
+
+})
+
+
+describe('Testing router carts', ()=>{
+    it('Endpoint POST de api/carts - Funcion de la creacion de un carrito', async()=>{
+        
+
+        const response = await requester.post('/api/carts')
+        const {_body} =response
+        
+        expect(_body.status).to.be.eq('success')
+
+    })
+
+    it('Endpoint GET de api/carts - traer carrito por su ID', async()=>{
+        
+
+        const response = await requester.get('/api/carts/64ec19491a23f8305b81e93e')
+        const {_body} =response
+    
+        expect(_body.status).to.be.eq('success')
+
+    })
+
+})
